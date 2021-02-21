7eb702e47665572a686d020c2c6d4362e6b05fc9
venky g <brawny.venky@gmail.com>
2021-02-21 11:22:53 +0000

    added classes and objects code

--- src/com/dummy/Flightpackage/Flight.java
+++ src/com/dummy/Flightpackage/Flight.java
@@ -7,19 +7,68 @@ package com.dummy.Flightpackage;
 // constructors(to run the initial setup as simple as initialization)
 public class Flight {
 
-    int passengers;
-    int seats;
+    //Fields store the object state.
+    private int passengers;
+    private int seats;
+    private String flightName;
 
-    Flight() {
+    //Constructor -
+    //Executable code to run automatically during the object creation.
+    //Sets classes initial state.
+    public Flight(String flightname) {
         passengers=0;
         seats=150;
+        flightName = flightname;
     }
 
-    void add1Passenger() {
+    //Encapsulation -
+    // accessor - mutator method
+    // ACCESSOR/GETTER - GET FIELD NAME
+    // SETTER/MUTATOR - SET FIELD NAME
+    // Accessors and Mutators - used to encapsulate the state inside classes
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats){
+        //<IMP> this keyword usage 
+        //we used this here as the compiler consider left handside seats as not class variable instead parameter variable
+        //so implicitly instead of asssigning classvariable = constructor parameter
+        //it will be constructor parameter = constructor paramter, which is wrong
+        //thats why we need to be explicitly add "this" keyword,(one important pattern for the usage of this keyword)
+        this.seats = seats;
+    }
+
+    //Methods
+    //executable code to manipulate state and perform operations.
+    public void add1Passenger() {
         if(passengers<seats) {
             passengers +=1;
+            System.out.println(flightName + " - number of passengers booked: "+ passengers);
+            System.out.println(flightName + " - total seats left : "+ (seats-passengers));
         }
-        System.out.println("number of passengers booked: "+ passengers);
-        System.out.println("total seats left : "+ (seats-passengers));
+        else {
+            handleTooMany();
+        }
+    }
+
+    //Methods
+    //executable code to manipulate state and perform operations.
+    public boolean hasRoom(Flight alteredflight) {
+        int total = this.passengers + alteredflight.passengers;
+        return total<=seats;
+    }
+
+    //Encapsulated Methods using access modifiers - private
+    //executable code to manipulate state and perform operations.
+    private void handleTooMany() {
+        System.out.println(flightName + " - flight overbooked");
+    }
+
+    //Methods
+    //executable code to manipulate state and perform operations.
+    public Flight createNewWithBoth(Flight combinedflight) {
+        this.passengers += combinedflight.passengers;
+        return this;
     }
 }
--- src/com/dummy/Flightpackage/MathEquation.java
+++ src/com/dummy/Flightpackage/MathEquation.java
@@ -1,4 +1,40 @@
 package com.dummy.Flightpackage;
 
+
 public class MathEquation {
+
+    //represents the state of the class
+    double leftVal;
+    double rightVal;
+    char opCode;
+    double result;
+
+    MathEquation(char opcode,double leftval,double rightval) {
+        leftVal=leftval;
+        rightVal = rightval;
+        opCode = opcode;
+    }
+    //operations
+    void execute() {
+        switch (opCode) {
+            case 'd':
+                result = rightVal!=0? leftVal/rightVal:0.0d;
+                break;
+            case 'a':
+                result = leftVal +rightVal;
+                break;
+            case 's':
+                result = leftVal-rightVal;
+                break;
+            case 'm':
+                result = leftVal*rightVal;
+                break;
+            default:
+                System.out.println("invalid opCode: " + opCode);
+                result = 0.0d;
+                break;
+        }
+    }
+
+
 }
